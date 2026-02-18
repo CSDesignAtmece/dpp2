ddf7c02ba98465b197cd20538f68fe1244b1fbe4
CSDesignAtmece <csdatme@gmail.com>
2026-02-18 10:22:59 +0530

    working

--- src/main/java/org/example/LoginTest.java
+++ src/main/java/org/example/LoginTest.java
@@ -1,9 +1,7 @@
-package org.example;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-public class loginTest {
+public class LoginTest {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com/");
@@ -18,3 +16,5 @@ public class loginTest {
         driver.quit();
     }
 }
+
+
